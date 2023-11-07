f923a7b46cc5314afb16c6aec185211bcf9fcbca
praveen <83015989+praveenctrl@users.noreply.github.com>
2023-11-07 18:32:42 +0530

    Add files via upload

---
+++ Bencelocomotives/abouts.js
@@ -0,0 +1,3 @@
+document.getElementById("img1").style.backgroundImage = "url('C:\Users\Admin\Desktop\Bencelocomotives\Bence - 9 - O Ring.png')";
+document.getElementById("img2").style.backgroundImage = "url('C:\Users\Admin\Desktop\Bencelocomotives\bence 33 3- Filter.png')";
+document.getElementById("img3").style.backgroundImage = "url('C:\Users\Admin\Desktop\Bencelocomotives\Bence 35 - Gasket.png')";
